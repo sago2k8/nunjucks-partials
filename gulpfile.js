26d8ab2b6ec4ee551b27e5d7092d30053c120b6c
Santiago Jimenez <sago2k8@gmail.com>
2019-05-29 19:29:35 -0500

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const gulp = require('gulp');
+const nunjucksRender = require('gulp-nunjucks-render');
+const sass = require('gulp-sass');
+const concat = require('gulp-concat');
+
+sass.compiler = require('node-sass');
+
+gulp.task('nunjucks', function() {
+  // Gets .html and .nunjucks files in pages
+  return gulp.src('app/pages/**/*.+(html|njk)')
+  // Renders template with nunjucks
+  .pipe(nunjucksRender({
+      path: ['app/templates']
+    }))
+  // output files in app folder
+  .pipe(gulp.dest('build/'))
+});
+
+gulp.task('sass', function() {
+  return gulp.src('./app/**/*.scss')
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(concat('styles.css'))
+    .pipe(gulp.dest('build/css'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('app/pages/**/*.+(html|njk)', gulp.serial('nunjucks'));
+  gulp.watch('./app/**/*.scss', gulp.serial('sass'));
+});
+
+gulp.task('default', gulp.parallel('nunjucks', 'sass'));
